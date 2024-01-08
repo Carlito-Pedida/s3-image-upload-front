4085820693170e3ad3b675831d267a8c408398bc
Carlito Pedida Jr <carlped417@gmail.com>
2024-01-07 23:25:17 -0600

    final mod

--- src/hooks/useMutation.js
+++ src/hooks/useMutation.js
@@ -1,24 +1,32 @@
-import { useState } from 'react';
-import axiosClient from '../config/axios';
+import { useState } from "react";
+import axiosClient from "../config/axios";
+import { Center, useToast } from "@chakra-ui/react";
 
-const useMutation = ({ url, method = 'POST' }) => {
+const useMutation = ({ url, method = "POST" }) => {
+  const toast = useToast();
   const [state, setState] = useState({
     isLoading: false,
-    error: '',
+    error: ""
   });
   // console.log(state);
 
-  const fn = async data => {
-    setState(prev => ({
+  const fn = async (data) => {
+    setState((prev) => ({
       ...prev,
-      isLoading: true,
+      isLoading: true
     }));
 
     axiosClient({ url, method, data })
       .then(() => {
-        setState({ isLoading: false, error: '' });
+        setState({ isLoading: false, error: "" });
+        toast({
+          title: "Successfully Added Image",
+          status: "success",
+          duration: 3000,
+          position: "top"
+        });
       })
-      .catch(error => {
+      .catch((error) => {
         setState({ isLoading: false, error: error.message });
       });
   };
